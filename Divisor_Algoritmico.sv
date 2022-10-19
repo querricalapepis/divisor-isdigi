e41d1754c846b6b18a7be60584dedd3acf32a3eb
Mario Rico Ibáñez <MRICIBA@upv.edu.es>
2022-10-19 09:23:00 +0200

    Primera Versión

---
+++ Divisor_Algoritmico.sv
@@ -0,0 +1,106 @@
+module Divisor_Algoritmico
+ 
+
+#(parameter tamanyo=32)           
+(input CLK,
+input RSTa,
+input Start,
+input logic [tamanyo-1:0] Num,
+input logic [tamanyo-1:0] Den,
+
+output logic [tamanyo-1:0] Coc,
+output logic [tamanyo-1:0] Res,
+output Done);
+
+
+//vuestro código
+
+
+logic [tamanyo-1 : 0] ACCU, Q, M;
+logic [$clog2(tamanyo) - 1 : 0] CONT;
+logic fin, SignDen, SignNum;
+
+enum int unsigned { D0 = 0, D1 = 2, D2 = 4, D3 = 8 } state;
+
+
+
+always_ff@(posedge CLK or negedge RSTa) begin
+	if(~RSTa) begin
+		state <= D0;
+		ACCU <= '0;
+		CONT <= '0;
+		SignNum <= '0;
+		SignDen <= '0;
+		Q <= '0;
+		M <= '0;
+		fin <= '0;
+		end
+	else 
+		case (state)
+
+		D0:
+			begin
+				fin <= '0;
+				if (Start) begin
+					state <= D1;
+					ACCU <= '0;
+					CONT <= tamanyo-1;
+					SignNum <= Num[tamanyo-1];
+					SignDen <= Num[tamanyo-1];
+					Q <= Num[tamanyo-1] ? (~Num+1) : Num;
+					M <= Den[tamanyo-1] ? (~Den+1) : Den;
+				end
+				else begin
+					state <= D0;
+				end
+			end
+		D1:
+			begin
+				state <= D2;
+				{ACCU,Q} <= {ACCU[tamanyo-2:0], Q, 1'b0};
+			end
+		D2:
+			begin
+			CONT <= CONT-1;
+			if (ACCU >= M) begin
+				Q <= Q + 1;
+				ACCU <= ACCU - M;
+			end else if(!CONT) begin
+				state <= D3;
+
+			end else begin
+				state <= D1; 
+			end
+			end
+		D3: begin
+				state <= D0;
+				fin <= 1'b1;
+				Coc <= (SignNum^SignDen) ? (~Q+1) : Q;
+				Res = SignNum ? (~ACCU+1) : ACCU;
+			end
+		endcase
+		
+	end
+
+
+assign Done = fin;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule 
---
+++ test_basico.sv
@@ -0,0 +1,54 @@
+localparam size = 32;
+`timescale 1ns/1ps
+
+module test_basico();
+
+	logic CLK;
+	logic RSTn;
+	logic START;
+	logic [size-1:0] NUM;
+	logic [size-1:0] DEN;
+
+	logic [size-1:0] COC;
+	logic [size-1:0] RES;
+	logic DONE;
+Divisor_Algoritmico DUV(CLK, RSTn, START, NUM, DEN, COC, RES, DONE);
+defparam DUV.tamanyo = 32;
+
+localparam T = 10;
+
+
+initial begin
+CLK = 0;
+forever #(T/2) CLK = !CLK;
+end
+
+initial 
+begin
+	START = 0;
+	NUM = 4;
+	DEN = 2;
+	reset(CLK,RSTn);
+	divide(CLK, START);
+    @(DONE);
+    #(T*2);
+    $finish;
+	
+end
+	
+	
+task automatic  reset(ref CLK, ref RESET);
+	@(negedge CLK)
+		RESET = 0;
+	@(negedge CLK)
+		RESET = 1;
+endtask
+
+task automatic  divide(ref CLK, ref START);
+	@(negedge CLK)
+		START = 1;
+	@(negedge CLK)
+		START = 0;
+endtask
+
+endmodule 
