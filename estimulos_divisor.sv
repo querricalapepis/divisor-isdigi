49654d4c2c7ed9f53c2d87d276e558bbeb8749e8
Sergio Moreno Suay <morenosuaysergio@gmail.com>
2022-11-10 19:31:18 +0100

    Fixed constrains

--- estimulos_divisor.sv
+++ estimulos_divisor.sv
@@ -2,14 +2,14 @@ class RandomInputGenerator #(parameter SIZE = 32);
 	randc logic [SIZE-1:0]  numerador;
 	randc logic [SIZE-1:0] denominador;
 
-	// constraint zeroRemainder {numerador % denominador == 0;}
-	// constraint notZeroRemainder {numerador % denominador != 0;}
+	constraint zeroRemainder {numerador % denominador == 0;}
+	constraint notZeroRemainder {numerador % denominador != 0;}
 
-    // constraint numeradorPositive {numerador[SIZE-1] == 0;}
-    // constraint numeradorNegative {numerador[SIZE-1] == 1;}
+    constraint numeradorPositive {numerador[SIZE-1] == 0;}
+    constraint numeradorNegative {numerador[SIZE-1] == 1;}
     	
-   	// constraint denominadorPositive {denominador[SIZE-1] == 0;}
-   	// constraint denominadorNegative {denominador[SIZE-1] == 1;}
+   	constraint denominadorPositive {denominador[SIZE-1] == 0;}
+   	constraint denominadorNegative {denominador[SIZE-1] == 1;}
 
 	function new();
 	begin
@@ -25,6 +25,12 @@ RandomInputGenerator #(.SIZE(SIZE)) randomInput;
 
 initial begin
 	randomInput = new();
+    randomInput.notZeroRemainder.constraint_mode(0);
+    randomInput.zeroRemainder.constraint_mode(0);
+    randomInput.numeradorPositive.constraint_mode(0);
+    randomInput.denominadorPositive.constraint_mode(0);
+    randomInput.numeradorNegative.constraint_mode(0);
+    randomInput.denominadorNegative.constraint_mode(0);
 	init();
     zeroRemainderDivisions();
     notZeroRemainderDivisions();
@@ -60,58 +66,58 @@ task newDivision();
 endtask
 
 task zeroRemainderDivisions();
-    //randomInput.notZeroRemainder.constraint_mode(0);
-    //randomInput.zeroRemainder.constraint_mode(1);
-    repeat(100) begin
+    randomInput.notZeroRemainder.constraint_mode(0);
+    randomInput.zeroRemainder.constraint_mode(1);
+    repeat(3) begin
         newDivision();
     end
 endtask
 
 task notZeroRemainderDivisions();
-    // randomInput.notZeroRemainder.constraint_mode(1);
-    // randomInput.zeroRemainder.constraint_mode(0);
-    repeat(100) begin
+    randomInput.notZeroRemainder.constraint_mode(1);
+    randomInput.zeroRemainder.constraint_mode(0);
+    repeat(3) begin
         newDivision();
     end
 endtask
 
 task positiveDivision1();
-    // randomInput.numeradorPositive.constraint_mode(1);
-    // randomInput.denominadorPositive.constraint_mode(1);
-    // randomInput.numeradorNegative.constraint_mode(0);
-    // randomInput.denominadorNegative.constraint_mode(0);
-    repeat(100) begin
+    randomInput.numeradorPositive.constraint_mode(1);
+    randomInput.denominadorPositive.constraint_mode(1);
+    randomInput.numeradorNegative.constraint_mode(0);
+    randomInput.denominadorNegative.constraint_mode(0);
+    repeat(3) begin
        newDivision();
     end
 endtask
 
 task positiveDivision2();
-    // randomInput.numeradorPositive.constraint_mode(0);
-    // randomInput.denominadorPositive.constraint_mode(0);
-    // randomInput.numeradorNegative.constraint_mode(1);
-    // randomInput.denominadorNegative.constraint_mode(1);
-    repeat(100) begin
+    randomInput.numeradorPositive.constraint_mode(0);
+    randomInput.denominadorPositive.constraint_mode(0);
+    randomInput.numeradorNegative.constraint_mode(1);
+    randomInput.denominadorNegative.constraint_mode(1);
+    repeat(3) begin
        newDivision();
     end
 endtask
 
 
 task negativeDivision1();
-    // randomInput.numeradorPositive.constraint_mode(1);
-    // randomInput.denominadorPositive.constraint_mode(0);
-    // randomInput.numeradorNegative.constraint_mode(0);
-    // randomInput.denominadorNegative.constraint_mode(1);
-    repeat(100) begin
+    randomInput.numeradorPositive.constraint_mode(1);
+    randomInput.denominadorPositive.constraint_mode(0);
+    randomInput.numeradorNegative.constraint_mode(0);
+    randomInput.denominadorNegative.constraint_mode(1);
+    repeat(3) begin
         newDivision();
     end
 endtask
 
 task negativeDivision2();
-    // randomInput.numeradorPositive.constraint_mode(0);
-    // randomInput.denominadorPositive.constraint_mode(1);
-    // randomInput.numeradorNegative.constraint_mode(1);
-    // randomInput.denominadorNegative.constraint_mode(0);
-    repeat(100) begin
+    randomInput.numeradorPositive.constraint_mode(0);
+    randomInput.denominadorPositive.constraint_mode(1);
+    randomInput.numeradorNegative.constraint_mode(1);
+    randomInput.denominadorNegative.constraint_mode(0);
+    repeat(3) begin
         newDivision();
     end
 endtask
